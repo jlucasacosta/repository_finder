e1d3fef63d3ad9a00d45e3349faea1dbe1445b96
jlucasacosta <luacostadelgado@gmail.com>
2023-10-18 08:06:31 -0300

    navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAppContext } from "../../context/AppContext";
 import style from "./nav.module.css";
 
 const Navigation = () => {
-
-  const { featuredRepositories } = useAppContext()
-
   return (
     <nav className={style.nav}>
       <Link onClick={() => {
